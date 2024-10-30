37aed642df24bf80d7f42e21d3189fca93af31de
Mr-afk-cell <kboxser2026@kehillahstudent.org>
2024-10-30 12:47:13 -0700

    excersise

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 '''
 Write a function `print_multiples` with one parameters `n`.
 
-When called, your function should print out ALL of the multiples of `n` between 0 and 100 (including both 0 and 100), and NO OTHER numbers.
+Whn called, your function should print out ALL of the multiples of `n` between 0 and 100 (including both 0 and 100), and NO OTHER numbers.
 
 Call your function for `n = 2`, `n = 3`, `n = 5`, and `n = 12`.
 '''
+def print_multiples(n):
+    for i in range(100):
+        if i % n == 0:
+            print(i)
+
+
+print_multiples(12)
